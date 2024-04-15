43259e5e9231d47fb80e3a668eb37b75953871c6
ficus2904 <ficus2904@gmail.com>
2024-04-15 23:44:37 +0900

    add cohere

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import asyncio
 import logging
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
+import cohere
 import json
 import sqlite3
 import atexit
+from time import time
 from aiogram import Bot, Dispatcher, F, types
 from aiogram.enums import ParseMode
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
@@ -15,6 +17,13 @@ warnings.simplefilter('ignore')
 # python app.py
 # git remote get-url origin # получить url для docker
 # api_keys = json.loads(os.getenv('api_key_bard_knows'))
+# self.buttons = {
+#         'Цитата': 'Напиши остроумную цитату. Цитата может принадлежать как реально существующей или существовавшей личности, так и вымышленного персонажа',
+#         'Шутка': 'Выступи в роли профессионального стендап комика и напиши остроумную шутку. Ответом должен быть только текст шутки',
+#         'Факт': 'Выступи в роли профессионального энциклопедиста и напиши один занимательный факт. Ответом должен быть только текст с фактом',
+#         'Квиз': '''Выступи в роли профессионального энциклопедиста и напиши три вопроса для занимательного квиза. 
+#                     Уровень вопросов: Старшая школа. Ответом должен быть только текст с тремя вопросами без ответов'''
+#     }
 api_keys = json.loads(open('./api_keys.json').read())
 logging.basicConfig(filename='./app.log', level=logging.INFO, encoding='utf-8',
                     format='%(asctime)19s %(levelname)s: %(message)s')
@@ -75,56 +84,148 @@ class GeminiAPI:
     
     def __init__(self):
         genai.configure(api_key=api_keys["gemini"])
-        self.model = genai.GenerativeModel('gemini-pro', safety_settings = {
+        self.model_name = 'gemini-pro'
+        self.model = genai.GenerativeModel(self.model_name, safety_settings = {
             HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
             HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
             HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
             HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE, 
         })
-        self.buttons = {
-                'Цитата': 'Напиши остроумную цитату. Цитата может принадлежать как реально существующей или существовавшей личности, так и вымышленного персонажа',
-                'Шутка': 'Выступи в роли профессионального стендап комика и напиши остроумную шутку. Ответом должен быть только текст шутки',
-                'Факт': 'Выступи в роли профессионального энциклопедиста и напиши один занимательный факт. Ответом должен быть только текст с фактом',
-                'Квиз': '''Выступи в роли профессионального энциклопедиста и напиши три вопроса для занимательного квиза. 
-                            Уровень вопросов: Старшая школа. Ответом должен быть только текст с тремя вопросами без ответов'''
-            }
+        self.context = []
 
     async def prompt(self, text: str) -> str:
-        response = self.model.generate_content(text)
+        self.context.append({'role':'user', 'parts':[text]})
+        response = self.model.generate_content(self.context)
+        self.context.append({'role':'model', 'parts':[response.text]})
         print(response.text)
         return escape(response.text)
 
 
+class CohereAPI:
+    """Singleton class for Gemini API"""
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(CohereAPI, cls).__new__(cls)
+        return cls._instance
+    
+    def __init__(self):
+        self.co = cohere.Client(api_keys["cohere"])
+        self.context = None
+
+    async def prompt(self, text):
+        response = self.co.chat(
+            model='command-r-plus',
+            chat_history=self.context,
+            message=text
+        )
+        self.context = response.chat_history
+        print(response.text)
+        return escape(response.text)
+
+
+class Agent:
+    ''' Router for agents'''
+    def __init__(self):
+        self.current = 'gemini'
+        self.buttons = {'Добавить в контекст универсальный промпт':'enrich', 
+                        'Показать текущий агент и размер контекста':'show',
+                        'Изменить агент':'change', 
+                        'Очистить контекст':'clear'}
+        self.universal_prompt = json.loads(open('./prompts.json').read())
+        self.all_prompts = 'https://vaulted-polonium-23c.notion.site/500-Best-ChatGPT-Prompts-63ef8a04a63c476ba306e1ec9a9b91c0'
+        self.time_dump = time()
+        
+    def enrich(self, option='0'):
+        if self.current == 'gemini':
+            gemini.context.extend([{'role':'user', 'parts':[self.universal_prompt[option][0]]},
+                                   {'role':'model', 'parts':[self.universal_prompt[option][1]]}])
+        elif self.current == 'cohere':
+            cohere_ins.context = [
+                {"role": "USER", "message": self.universal_prompt[option][0]},
+                {"role": "CHATBOT", "message": self.universal_prompt[option][1]}
+            ]
+
+        
+    def show(self) -> int:
+        if self.current == 'gemini':
+            length = 0 if not gemini.context else len(gemini.context)
+        elif self.current == 'cohere':
+            length = 0 if not cohere_ins.context else len(cohere_ins.context)
+        return length
+
+    def change(self):
+        if self.current == 'gemini':
+            self.current = 'cohere'
+        elif self.current == 'cohere':
+            self.current = 'gemini'
+
+    def clear(self):
+        cohere_ins.context = None
+        gemini.context = []
+
+    async def prompt(self, text):
+        if self.current == 'gemini':
+            output = await gemini.prompt(text)
+        elif self.current == 'cohere':
+            output = await cohere_ins.prompt(text)
+        return output
+
+
 db = DBConnection()
 gemini = GeminiAPI()
+cohere_ins = CohereAPI()
 dp = Dispatcher()
+agent = Agent()
 
 builder = ReplyKeyboardBuilder()
-for display_text in gemini.buttons:
+for display_text in agent.buttons:
     builder.button(text=display_text)
 builder.adjust(2, 2)
 
 
 @dp.message(F.content_type.in_({'text'}))
 async def echo_handler(message: types.Message | types.KeyboardButtonPollType):
-    # input_data = arr_api_data.get(message.text, 'random')
+    ## clear context after 15 minutes
+    if (time() - agent.time_dump) > 900:
+        agent.clear()
+    agent.time_dump = time()
     user_name = db.check_user(message.from_user.id)
     logging.info(f'{user_name[1] if user_name else message.from_user.id}: "{message.text}"')
-    text = gemini.buttons.get(message.text, message.text)
-
-    if user_name is None:
-        print(f'Новый пользователь: {user_name[0]}')
-        await message.reply('Доступ запрещен. Обратитесь к администратору')
-        return
-    if text == '/start':
-        await message.reply(f'Доступ открыт. Добро пожаловать {user_name[1]}!')
-        return
+    text = agent.buttons.get(message.text, message.text)
 
     try:
-        # output = await handler(input_data)
-        await message.reply('Ожидайте...')
-        output = await gemini.prompt(text)
+        if user_name is None:
+            print(f'Новый пользователь: {user_name[0]}')
+            await message.reply('Доступ запрещен. Обратитесь к администратору')
+            return
+        if text == '/start':
+            await message.reply(f'Доступ открыт. Добро пожаловать {user_name[1]}!')
+            return
+        if text in agent.buttons.values() or text in '1':
+            match text:
+                case 'enrich':
+                    agent.enrich()
+                    output = f'Универсальный контекст добавлен в {agent.current}. [Дополнительные промпты]({agent.all_prompts})'
+                case '1':
+                    agent.enrich('1')
+                    output = 'Роль учителя английского языка начата.'
+                case 'show':
+                    output = f'Текущий агент: {agent.current}, размер контекста {agent.show()}'
+                case 'change':
+                    agent.change()
+                    output = f'Смена агента на {agent.current}'
+                case 'clear':
+                    agent.clear()
+                    output = 'Контекст диалога отчищен'
+            output = escape(output)
+            # await message.answer(output, reply_markup=builder.as_markup(), parse_mode=ParseMode.MARKDOWN_V2)
+        else:
+            await message.reply('Ожидайте...')
+            output = await agent.prompt(text)
         await message.answer(output, reply_markup=builder.as_markup(), parse_mode=ParseMode.MARKDOWN_V2)
+        return
     except Exception as e:
         await message.answer(str(e))
         return
