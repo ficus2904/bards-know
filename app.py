c46fa915ffc590de3f3fdf473f565fc8f05c78fe
ficus2904 <ficus2904@gmail.com>
2024-08-07 21:18:28 +0900

    fix max caption len

--- app.py
+++ app.py
@@ -738,11 +738,11 @@ async def image_handler(message: types.Message):
         elif isinstance(kwargs, str):
             match = re.search(r'https://[^"]+\.jpg', kwargs)
             kwargs = {"photo":match.group(0) if match else None,
-                      "caption":kwargs.split('"caption":"')[-1].rstrip('"}')}
+                      "caption":kwargs.split('"caption":"')[-1].rstrip('"}')[:1000]}
         if kwargs['photo']:
             await message.reply_photo(**kwargs)
         else:
-            await message.reply(kwargs['caption'])
+            await message.reply(kwargs['caption'][:1000])
     except Exception as e:
         await message.reply(f"An error occurred: {e}. {kwargs}")
 
