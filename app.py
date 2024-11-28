a3d7625d0581a71c188bd4c6dee7253ffb8b4d80
ficus2904 <ficus2904@gmail.com>
2024-11-28 15:56:25 +0900

    add new model (llama-3.1-70b-specdec) to Groq

--- app.py
+++ app.py
@@ -247,6 +247,7 @@ class GroqAPI(BaseAPIInterface):
             'llama-3.2-11b-vision-preview',
             'llama-3.2-90b-vision-preview',
             'llama-3.1-70b-versatile',
+            'llama-3.1-70b-specdec',
             'llama3-70b-8192',
             'llama3-groq-70b-8192-tool-use-preview',
             'llava-v1.5-7b-4096-preview',
