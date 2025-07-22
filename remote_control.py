503fb99d42b22708ae2be574abde65f83653bf21
ficus2904 <ficus2904@gmail.com>
2025-07-22 14:48:40 +0000

    fix

--- remote_control.py
+++ remote_control.py
@@ -15,20 +15,20 @@ class RemoteControl():
     
     port: str = '/dev/ttyS5'
     def __init__(self):
-        self.tv_code: str = 'a1 f1 01 01 a2'  # Пример кода для отправки
-        self.audio_code: str = 'a1 f1 01 02 a2'  # Пример кода для отправки аудио
+        self.tv_switch: str =    'A1 F1 04 fb 08'
+        self.audio_switch: str = 'A1 F1 02 fd 48'
 
     
     def command_router(self, cmd: str | None = None) -> str:
         """Маршрутизатор команд для отправки ИК-команд."""
         match cmd:
             case 'tv':
-                output = self._send_ir(self.tv_code)
+                output = self._send_ir(self.tv_switch)
             case 'audio':
-                output = self._send_ir(self.audio_code)
+                output = self._send_ir(self.audio_switch)
             case None:
-                output = self._send_ir(self.tv_code)
-                output = self._send_ir(self.audio_code)
+                output = self._send_ir(self.tv_switch)
+                output = self._send_ir(self.audio_switch)
             case _:
                 output = f"❌ Неизвестная команда: {cmd}"
         
@@ -38,12 +38,10 @@ class RemoteControl():
     def _send_ir(self, hex_code_string: str) -> str:
         """Отправляет ИК-команду, представленную в виде hex-строки."""
         try:
-            # Преобразуем строку hex в байты
-            # Например, 'a1 f1 01 01 a2' -> b'\xa1\xf1\x01\x01\xa2'
             command_bytes = bytes.fromhex(hex_code_string)
 
             with serial.Serial(self.port, 9600, timeout=1) as ser:
-                print(f"⚠️ Отправка команды: {hex_code_string}")
+                logger.info(f"⚠️ Отправка команды: {hex_code_string}")
                 ser.write(command_bytes)
                 output = "✅ Команда отправлена."
 
@@ -74,10 +72,40 @@ class RemoteControl():
             print("\n⚠️ Программа завершена.")
 
 
+    @classmethod
+    def _test(cls) -> bool:
+        # Собираем полную команду
+        full_command = 'A1 F1 04 fb 08'
+        print(f"⚠️ Send: '{full_command}'...")
+        try:
+            command_bytes = bytes.fromhex(full_command.replace(' ', ''))
+            
+            if len(command_bytes) != 5:
+                print(f"❌ Ошибка сборки, итоговая команда не 5 байт.")
+                return False
+
+            with serial.Serial(cls.port) as ser:
+                ser.write(command_bytes)
+
+            print("✅ Success!")
+
+        except Exception as e:
+            print(f"❌Произошла ошибка при отправке: {e}")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='IR Remote Control')
     parser.add_argument('--read', action='store_true', help='Read IR codes')
+    parser.add_argument('--send', action='store_true', help='Send IR codes')
     args = parser.parse_args()
     if args.read:
         RemoteControl._read_ir()
+    elif args.send:
+        RemoteControl._test()
         # uv run remote_control.py --read
+    ## add to docker-compose.yml
+    # devices:
+    #   - "/dev/ttyS5:/dev/ttyS5"
+    # group_add:
+    #   - "20" 
+    ## and pyserial to requirements.txt
